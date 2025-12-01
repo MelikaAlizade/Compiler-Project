d7c8ec0f9b0b9aa2e4522b09786c63dcffd9bde3
melikaalizade <melikaalizadeh7@gmail.com>
2025-12-01 03:43:10 +0330

    add main functionality of the scanner

--- Compiler.py
+++ Compiler.py
@@ -29,6 +29,194 @@ def initialize():
         input_text = ''
 
 
+def get_next_token():
+    global lineno, errors
+
+    skip_whitespace()
+
+    if current_char() is None:
+        return None
+
+    if current_char() == '/' and lookahead_char() in ['/', '*']:
+        read_comment()
+        return get_next_token()
+
+    token_line = lineno
+    ch = current_char()
+
+    if ch == '*' and lookahead_char() == '/':
+        errors.append((token_line, '*/', 'Stray closing comment'))
+        advance()
+        advance()
+        return get_next_token()
+
+    if is_digit(ch):
+        return read_number(token_line)
+
+    if is_letter(ch):
+        return read_identifier(token_line)
+
+    if ch in ['(', ')', '[', ']', '{', '}', ';', ':', ',', '+', '-', '*', '/']:
+        advance()
+        return (token_line, 'SYMBOL', ch)
+
+    if ch == '=':
+        advance()
+        if current_char() == '=':
+            advance()
+            return (token_line, 'SYMBOL', '==')
+        return (token_line, 'SYMBOL', '=')
+
+    if ch == '<':
+        advance()
+        return (token_line, 'SYMBOL', '<')
+
+    errors.append((token_line, ch, 'Illegal character'))
+    advance()
+    return get_next_token()
+
+
+def skip_whitespace():
+    while current_char() and is_whitespace(current_char()):
+        advance()
+
+
+def is_whitespace(ch):
+    return ch in [' ', '\n', '\r', '\t', '\v', '\f']
+
+
+def current_char():
+    global pos, input_text
+
+    if pos < len(input_text):
+        return input_text[pos]
+
+    return None
+
+
+def lookahead_char(offset=1):
+    global pos, input_text
+
+    if pos + offset < len(input_text):
+        return input_text[pos + offset]
+    return None
+
+
+def advance():
+    global pos, lineno, input_text
+
+    if pos < len(input_text):
+        if input_text[pos] == '\n':
+            lineno += 1
+
+        pos += 1
+
+
+def read_comment():
+    global lineno, pos, input_text, errors
+
+    start_line = lineno
+
+    if current_char() == '/' and lookahead_char() == '/':
+        advance()  # skip first /
+        advance()  # skip second /
+
+        while current_char() and current_char() not in ['\n', '\f']:
+            advance()
+
+        if current_char() == '\f':
+            advance()
+
+        return True
+
+    elif current_char() == '/' and lookahead_char() == '*':
+        advance()  # skip /
+        advance()  # skip *
+
+        comment_start = pos - 2
+
+        while True:
+            if current_char() is None:
+                unclosed_text = input_text[comment_start:min(
+                    comment_start + 7, len(input_text))]
+                if len(input_text) - comment_start > 7:
+                    unclosed_text += '…'
+                errors.append(
+                    (start_line, unclosed_text, 'Open comment at EOF'))
+                return True
+
+            if current_char() == '*' and lookahead_char() == '/':
+                advance()  # skip *
+                advance()  # skip /
+                return True
+
+            advance()
+
+    return False
+
+
+def is_digit(ch):
+    return ch and ch.isdigit()
+
+
+def read_number(token_line):
+    global pos, errors
+
+    start_pos = pos
+    num_str = ''
+
+    while is_digit(current_char()):
+        num_str += current_char()
+        advance()
+
+    if is_letter(current_char()):
+        error_str = num_str
+        while is_id_char(current_char()):
+            error_str += current_char()
+            advance()
+        errors.append((token_line, error_str, 'Malformed number'))
+        return get_next_token()
+
+    if len(num_str) > 1 and num_str[0] == '0':
+        errors.append((token_line, num_str, 'Malformed number'))
+        return get_next_token()
+
+    return (token_line, 'NUM', num_str)
+
+
+def is_letter(ch):
+    return ch and (ch.isalpha() or ch == '_')
+
+
+def is_id_char(ch):
+    return ch and (ch.isalnum() or ch == '_')
+
+
+def read_identifier(token_line):
+    global symbol_table, errors
+    id_str = ''
+
+    while is_id_char(current_char()):
+        id_str += current_char()
+        advance()
+
+    if current_char() and not is_whitespace(current_char()) and \
+       current_char() not in ['(', ')', '[', ']', '{', '}', ';', ':', ',', '+', '-', '*', '/', '=', '<'] and \
+       not is_digit(current_char()) and not is_letter(current_char()):
+        error_str = id_str + current_char()
+        errors.append((token_line, error_str, 'Illegal character'))
+        advance()
+        return get_next_token()
+
+    if id_str in keywords:
+        return (token_line, 'KEYWORD', id_str)
+
+    if id_str not in symbol_table:
+        symbol_table.append(id_str)
+
+    return (token_line, 'ID', id_str)
+
+
 def scan():
     global tokens
 
